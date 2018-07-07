a16a33b00e561792ae5f4efad9063ce03ddf311e
Bear14 <bearlord14@gmail.com>
2018-07-07 11:25:44 +0200

    Port for Linux

--- src/Generator.h
+++ src/Generator.h
@@ -14,11 +14,11 @@ class Generator {
 private:
     std::vector<std::string> alphabet;
 
-    const std::array<std::string, 26> lowerCase = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                                                   "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    const std::array<std::string, 26> upperCase = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-                                                   "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    const std::array<std::string, 10> numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    const std::array<std::string, 26> lowerCase = {{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                                                   "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}};
+    const std::array<std::string, 26> upperCase = {{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                                                   "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}};
+    const std::array<std::string, 10> numbers = {{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}};
     std::vector<std::string> special = {{std::string(
             "!"), "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"}};
 
